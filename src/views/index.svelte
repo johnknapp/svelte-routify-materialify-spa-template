4e24a3944028014ac504d0898ee23a3ae954fa31
John Knapp <jk@johnknapp.com>
2021-04-04 17:13:37 -0700

    link to css and add a test component

--- src/views/index.svelte
+++ src/views/index.svelte
@@ -1,7 +1,10 @@
 <script>
+  import { Chip } from 'svelte-materialify';
   import {url} from '@roxi/routify'
 </script>
 
 <h1>i m index</h1>
 
 <a href={$url('/pizza')}>Go for pizza</a>
+
+<Chip class="ma-2 primary-color">You can call me chip</Chip>
