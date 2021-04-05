ebae764798c7ff8bdb647abaae75a34ff5f2dad1
John Knapp <jk@johnknapp.com>
2021-04-04 18:54:57 -0700

    update path

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ const sveltePreprocess = require('svelte-preprocess')
 module.exports = {
   preprocess: sveltePreprocess({
     scss: {
-      includePaths: ["src/styles"],
+      includePaths: ['src/theme'],
     }
   })
 }
